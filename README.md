a8fc579062df403ef5d1a14f8654cd2926cbba6a
ioritztubio <itubio002@ikasle.ehu.eus>
2025-01-21 00:40:58 +0100

    Mobile Header and Bedroom started, Header finished

---
+++ README.md
@@ -0,0 +1,9 @@
+# 🏡 Página web responsive para el agroturismo Arraspine ubicado en astigarraga.
+
+- Pagina web responsive utilizando HTML, CSS & JavaScript
+- Contiene animaciones de scroll.
+- Incluye los dos modos claro y oscuro.
+- Scroll suave para cada sección.
+- Desarrollada con la metodologia Mobile First.
+- Compatible con todos los dispositivos.
+
---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader)
+/*=============== SWIPER POPULAR ===============*/
+let swiper = new Swiper(".bedroom__container", {
+    spaceBetween: 30,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+        },
+    });
+
+/*=============== VALUE ACCORDION ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
