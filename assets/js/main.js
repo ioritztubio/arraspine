a3612aa84339cf7c02a0819f5cdd40af35d61d1e
ioritztubio <itubio002@ikasle.ehu.eus>
2025-01-26 01:14:36 +0100

    breakpoints treated

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,3 +117,18 @@ themeButton.addEventListener("click", () => {
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+sr.reveal(`.home__title, .bedroom__container, footer__container`)
+sr.reveal(`.home__description, .footer__info`, {delay: 500})
+sr.reveal(`.home__value`, {delay: 600})
+sr.reveal(`.home__images`, {delay: 800, origin: "bottom"})
+sr.reveal(`.value__images, .contact__content`, {interval: 100}, {origin: "left"})
+sr.reveal(`.home__social-icon`, {interval: 100, origin: "right"})
+sr.reveal(`.value__images, .contact__content`, {origin: "left"})
+sr.reveal(`.value__content, .contact__images`, {origin: "right"})
