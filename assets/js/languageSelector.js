8ececa19982cff46182219afc5307ebc77ddc231
ioritz <ioritz@itubio002@ikasle.ehu.eus>
2025-01-28 16:56:27 +0100

    translations almost done

--- assets/js/languageSelector.js
+++ assets/js/languageSelector.js
@@ -9,7 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 setLanguage = (language) => {
-  console.log(language);
   translateSections(language);
 };
 
@@ -21,7 +20,18 @@ async function loadTranslations() {
 
 async function translateSections(selectedLanguage) {
   const data = await loadTranslations();
-  console.log(selectedLanguage);
+
+  //Header section
+  document.getElementById("trans__home").innerHTML =
+    data.header.home[selectedLanguage];
+  document.getElementById("trans__rooms").innerHTML =
+    data.header.rooms[selectedLanguage];
+  document.getElementById("trans__value").innerHTML =
+    data.header.values[selectedLanguage];
+  document.getElementById("trans__contact").innerHTML =
+    data.header.contact[selectedLanguage];
+  document.getElementById("trans__reserve").innerHTML =
+    data.header.reserve[selectedLanguage];
 
   //Home section
   document.getElementById("trans_title").innerHTML =
@@ -37,12 +47,17 @@ async function translateSections(selectedLanguage) {
 
   //Rooms section
 
+  document.getElementById("rooms__title").innerHTML =
+    data.rooms.title[selectedLanguage];
+  document.getElementById("rooms__description").innerHTML =
+    data.rooms.description[selectedLanguage];
   const container = document.querySelector(".swiper-wrapper");
 
+  container.innerHTML = "";
+
   data.rooms.rooms.forEach((room) => {
-    console.log('room:', room);
-    const article = document.createElement('article');
-    article.className = 'bedroom__card swiper-slide';
+    const article = document.createElement("article");
+    article.className = "bedroom__card swiper-slide";
     article.id = room.id;
 
     article.innerHTML = `
@@ -57,11 +72,10 @@ async function translateSections(selectedLanguage) {
     container.appendChild(article);
   });
 
-
+  // Apartment section
   data.rooms.apartment.forEach((apartment) => {
-    console.log('apartment:', apartment);
-    const article = document.createElement('article');
-    article.className = 'bedroom__card swiper-slide';
+    const article = document.createElement("article");
+    article.className = "bedroom__card swiper-slide";
     article.id = apartment.id;
 
     article.innerHTML = `
@@ -74,6 +88,57 @@ async function translateSections(selectedLanguage) {
     `;
 
     container.appendChild(article);
-  }
-  );
+  });
+
+  //Values section
+  document.getElementById("values__title").innerHTML =
+    data.values.title[selectedLanguage];
+  document.getElementById("values__subtitle").innerHTML =
+    data.values.subtitle[selectedLanguage];
+  document.getElementById("values__description").innerHTML =
+    data.values.description[selectedLanguage];
+  document.getElementById("value__accordion-title1").innerHTML =
+    data.values.accordion[0].title[selectedLanguage];
+  document.getElementById("value__accordion-description1").innerHTML =
+    data.values.accordion[0].description[selectedLanguage];
+  document.getElementById("value__accordion-title2").innerHTML =
+    data.values.accordion[1].title[selectedLanguage];
+  document.getElementById("value__accordion-description2").innerHTML =
+    data.values.accordion[1].description[selectedLanguage];
+  document.getElementById("value__accordion-title3").innerHTML =
+    data.values.accordion[2].title[selectedLanguage];
+  document.getElementById("value__accordion-description3").innerHTML =
+    data.values.accordion[2].description[selectedLanguage];
+  document.getElementById("value__accordion-title4").innerHTML =
+    data.values.accordion[3].title[selectedLanguage];
+  document.getElementById("value__accordion-description4").innerHTML =
+    data.values.accordion[3].description[selectedLanguage];
+  document.getElementById("value__accordion-title5").innerHTML =
+    data.values.accordion[4].title[selectedLanguage];
+  document.getElementById("value__accordion-description5").innerHTML =
+    data.values.accordion[4].description[selectedLanguage];
+  document.getElementById("value__accordion-title6").innerHTML =
+    data.values.accordion[5].title[selectedLanguage];
+  document.getElementById("value__accordion-description6").innerHTML =
+    data.values.accordion[5].description[selectedLanguage];
+
+  //Contact section
+  document.getElementById("contact__title").innerHTML =
+    data.contact.title[selectedLanguage];
+  document.getElementById("contact__subtitle").innerHTML =
+    data.contact.subtitle[selectedLanguage];
+  document.getElementById("contact__description").innerHTML =
+    data.contact.description[selectedLanguage];
+  document.querySelectorAll(".trans__contact__call").forEach((element) => {
+    element.innerHTML = data.contact.call[selectedLanguage];
+  });
+
+  document.querySelectorAll(".trans__contact__call-now").forEach(element => {element.innerHTML =  data.contact.cnow[selectedLanguage]});
+  document.getElementById('contact__chat').innerHTML = data.contact.chat[selectedLanguage];
+  document.getElementById('contact__chat-now').innerHTML = data.contact.chnow[selectedLanguage];
+  document.getElementById('contact__reserve').innerHTML = data.contact.reserve[selectedLanguage];
+  document.getElementById('contact__reserve-now').innerHTML = data.contact.rnow[selectedLanguage];  
+
+  //Footer section
+  document.getElementById('footer__description').innerHTML = data.footer.description[selectedLanguage];
 }
